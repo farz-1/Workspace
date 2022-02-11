9d597d99c5d583f302fd796f2f8bd66e4e1801ba
“Farzwan <2553017m@student.gla.ac.uk>
2022-02-11 15:29:04 +0000

    ch4&5 done

--- rango/views.py
+++ rango/views.py
@@ -4,10 +4,12 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!<a href='/rango/about/'>About</a>")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.<a href='/rango/'>About</a>")
+    context_dict = {'name': 'Farzwan Mohamed'}
+    return render(request, 'rango/about.html', context=context_dict)
 
 # Create your views here.
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -26,6 +26,9 @@ SECRET_KEY = 'lxiuw7w^53zj*b62ylw5=imjqlkiw4lpr_7*b&9j$m2*z@ts=2'
 DEBUG = True
 
 ALLOWED_HOSTS = []
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 # Application definition
@@ -55,7 +58,8 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -99,6 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 
 # Internationalization
@@ -119,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
