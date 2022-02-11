b01757e1252003adca14ecf3a53aa70a3d557074
“Farzwan <2553017m@student.gla.ac.uk>
2022-02-11 15:21:18 +0000

    ch3 done

--- rango/urls.py
+++ rango/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from rango import views
+from django.urls import include
 
 app_name = 'rango'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about,name='about'),
 ]
